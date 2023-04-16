b9ff1b14048d4b0b57b669fce7f439ca75fa39f0
Aryan <38075578+aryan877@users.noreply.github.com>
2023-04-17 00:30:25 +0530

    added vanalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import theme from '../../theme';
 import '../styles/globals.css';
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
